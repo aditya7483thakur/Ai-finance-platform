c218406feaa2d6b448d31a337a871a50e0697a15
Aditya <adityakumarji74@gmail.com>
2025-04-18 23:16:56 +0530

    changes done

--- server/app.js
+++ server/app.js
@@ -23,11 +23,6 @@ app.use(
     credentials: true,
   })
 );
-app.use((req, res, next) => {
-  console.log("Incoming Request Headers:");
-  console.log(req.headers.authorization);
-  next();
-});
 
 app.use(arcjetMiddleware);
 app.use(clerkMiddleware({ debug: true }));
